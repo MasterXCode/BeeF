265de0684190388ea39ae8d9973f30290b5e3bc4
MASTERX <50529518+seifsherifssss@users.noreply.github.com>
2019-07-11 16:59:51 +0200

    Add files via upload

---
+++ beef.sh
@@ -0,0 +1,27 @@
+pkg install toilet figlet ruby && gem install lolcat 
+clear
+toilet -f big ' BeeF ' | lolcat 
+echo""
+echo ' Best Method to Install BeeF Working 100💯💯 '
+echo ""
+################
+echo "     Installating"
+################
+
+pkg install python python2 git ruby perl php curl -y 
+
+ tar -xvzf BeeF-0.4.7.0.tar.gz 
+
+  mv -fin beef-xss /$PREFIX/share && cd /$PREFIX/share/beef-xss 
+ 
+touch /$PREFIX/bin/beef && chmod 777 /$PREFIX/bin/beef && echo ' bash  /$PREFIX/share/beef-xss/beef ' >  /$PREFIX/bin/beef 
+
+gem install bundle bundler && pip install bundler bundle && pip2 install bundler bundle && bundle install  
+
+
+cd /$HOME/BeeF && bash FixBigdecimal.sh 
+
+echo '   Now You Can Run BeeF by Typing ====>>  beef   '
+echo '' 
+toilet -f small ' Enjoy...! ' | lolcat 
+
